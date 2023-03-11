54a9b1a8325948bca13746a0bace87e89956855a
Rocky Essel <rockyessel76@gmail.com>
2023-03-11 00:06:44 +0000

    rust

--- src/components/atoms/text_input.rs
+++ src/components/atoms/text_input.rs
@@ -1,17 +1,25 @@
 use gloo::console::log;
+use wasm_bindgen::JsCast;
+use web_sys::HtmlInputElement;
+// use wasm_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
     pub name: String,
+    pub handle_onchange: Callback<String>,
 }
 
 #[function_component(TextInput)]
 pub fn text_input(props: &Props) -> Html {
-    let on_change = Callback::from(|event: Event| {
-        let target = event.target();
-        // let input = event.unchecked
-        log!("target", target);
+    let handle_onchange = props.handle_onchange.clone();
+    let on_change = Callback::from( move|event: Event| {
+        let value = event
+            .target()
+            .unwrap()
+            .unchecked_into::<HtmlInputElement>()
+            .value();
+        handle_onchange.emit(value)
     });
 
     html! {
